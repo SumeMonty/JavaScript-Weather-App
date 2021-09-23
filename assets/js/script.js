cd5b13a5bc34fc5206e17beab035624a6d3818b1
Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>
2021-09-23 15:30:34 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+const cityElement = document.querySelector(".city");
+const iconElement = document.querySelector(".icon");
+const tempElement = document.querySelector(".temp");
+const descElement = document.querySelector(".description");
+const humidityElement = document.querySelector(".humidity");
+const windElement = document.querySelector(".wind");
+const weatherElement = document.querySelector(".weather");
+const searchBtnElement = document.querySelector(".search button");
+const searchBarElement = document.querySelector(".search-bar");
 let weather = {
   apiKey: "98089c814ceb7377bc0bbd0ba68651ec",
   fetchWeather: function (city) {
@@ -19,53 +28,51 @@ let weather = {
 };
 
 function displayWeather(data) {
-  const name  = data.name;
-  const icon  = data.weather[0].icon;
-  const  description  = data.weather[0].description;
-  const temp  = data.main.temp;
-  const  humidity  = data.main.humidity;
-  const speed  = data.wind.speed;
-  document.querySelector(".city").innerHTML = `Weather in ${name}`;
-  document.querySelector(".icon").src =
+  const name = data.name;
+  const icon = data.weather[0].icon;
+  const description = data.weather[0].description;
+  const temp = data.main.temp;
+  const humidity = data.main.humidity;
+  const speed = data.wind.speed;
+  cityElement.innerHTML = `Weather in ${name}`;
+  iconElement.src =
     `https://openweathermap.org/img/wn/${icon}.png`;
-  document.querySelector(".description").innerHTML = `${description}`;
-  document.querySelector(".temp").innerHTML = `${temp}<span>°C</span>`;
-  document.querySelector(".humidity").innerHTML =
+  descElement.innerHTML = `${description}`;
+  tempElement.innerHTML = `${temp}<span>°C</span>`;
+  humidityElement.innerHTML =
     `Humidity: ${humidity}%`;
-  document.querySelector(".wind").innerHTML =
+  windElement.innerHTML =
     `Wind speed: ${speed} <span>km/h</span>`;
-  document.querySelector(".weather").classList.remove("loading");
+  weatherElement.classList.remove("loading");
   document.body.style.backgroundImage =
     `url('https://source.unsplash.com/1600x900/?${description} weather')`;
   // document.body.style.backgroundImage = url(`../img/${description}.jpg`);
 }
 
-document.querySelector(".search button").addEventListener("click", function () {
+searchBtnElement.addEventListener("click", function () {
   weather.search();
 });
 
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+searchBarElement.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
 
 
 //Checking Current Location Of The User
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
-if('geolocation' in navigator){
+if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(setPosition, showError);
-}else{
+} else {
   alert("Browser doesn't Support Geolocation");
 }
 
 
 
 // SET USER'S POSITION
-function setPosition(position){
+function setPosition(position) {
   let lat = position.coords.latitude;
   let lng = position.coords.longitude;
   fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`)
@@ -83,7 +90,7 @@ function detLocation(locationJson) {
 }
 
 // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
-function showError(error){
+function showError(error) {
   notificationElement.style.display = "block";
   notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
